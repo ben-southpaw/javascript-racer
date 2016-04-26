35e412f89e78741026723e0b8a4e0c5282846b45
ben-southpaw <ben.dryden@gmail.com>
2016-04-26 14:31:15 +1200

    Add files and grid completed

---
+++ javascript-racer.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){}
+
+
+  /*Add keypress event 
+  */
+  var el = document.getElementById("outside");
+el.addEventListener("click", modifyText, false);
